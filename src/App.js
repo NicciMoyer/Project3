29ea3cd313d760f25cbb18d8d654a02d294d780a
Nicci Moyer <niccihendrix@yahoo.com>
2020-09-24 13:25:57 -0500

    teacher and class landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Books from "./pages/Books"
+
+
 
 function App() {
   return (
     <Router>
     <div className="App">
 
-      <Route exact path = "/" component={Books}/>
+      <Route exact path = "/" component={}/>
 
     </div>
     </Router>
---
+++ src/teacherLanding/teacherLanding.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+// import API from "../utils/API";
+import { Link } from "react-router-dom";
+import coloredPencilsBottom from "../images/colored-pencils-bottom.jpg";
+
+
+// function students() {
+// Setting our component's initial state
+// const [Students, setStudents] = useState([])
+// const [formObject, setFormObject] = useState({})
+// // Load all students and store them with setStudents
+// useEffect(() => {
+//   loadStudents()
+// }, [])
+
+// // Loads all students and sets them to students
+// function loadStudents() {
+//   API.getStudents()
+//     .then(res => 
+//       setStudents(res.data)
+//     )
+//     .catch(err => console.log(err));
+// };
+
+// Deletes a student from the database with a given id, then reloads students from the db
+// function deleteStudent(id) {
+  // API.deleteStudent(id)
+  //   .then(res => loadStudents())
+  //   .catch(err => console.log(err));
+// }
+
+// Handles updating component state when the user types into the input field
+// function handleInputChange(event) {
+  // const { name, value } = event.target;
+  // setFormObject({...formObject, [name]: value})
+// };
+
+// When the form is submitted, use the API.saveStudent method to save the student data
+// Then reload students from the database
+// function handleFormSubmit(event) {
+  // event.preventDefault();
+  // if (formObject.title && formObject.author) {
+  //   API.saveStudent({
+  //     title: formObject.title,
+  //     author: formObject.author,
+  //     synopsis: formObject.synopsis
+  //   })
+  //     .then(res => loadStudents())
+  //     .catch(err => console.log(err));
+  // }
+// };
+
+return (
+  <div styles={{ backgroundImage: `url(${coloredPencilsBottom})` }}>
+    
+      <h1>Welcome, Ms. Frizzle!</h1>
+    
+
+    <row>
+      <col className="classcol" size="col-md-2 col-12">
+
+        <h3>My Classes</h3>
+        <br></br>
+        <ul>
+          <li>Biology</li>
+          <li>Anatomy</li>
+          <li>Biology II</li>
+
+        </ul>
+      </col>
+      <col className="Studentscol" size="col-md-5 col-12">
+
+        <h2>Students</h2>
+        <br></br>
+        <ul>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+          <li>Student</li>
+        </ul>
+
+      </col>
+      <col className="assignmentscol" size="col-md-5 col-12">
+
+        <h2>Assignments</h2>
+        <br></br>
+        <ul>
+          <li>Assignment</li>
+          <li>Assignment</li>
+          <li>Assignment</li>
+          <li>Assignment</li>
+          <li>Assignment</li>
+        </ul>
+
+
+      </col>
+    </row>
+  </div>
+);
+
+export default teacherLanding;
