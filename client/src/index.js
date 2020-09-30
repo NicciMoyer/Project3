fdf33b12f8b206c11b45dc813dcb3a3547285e8a
Nicci Moyer <niccihendrix@yahoo.com>
2020-09-30 13:24:20 -0500

    sign up form

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+// import coloredPencilsBottom from "../public/images/coloredPencilsBottom.jpg";
 
 ReactDOM.render(
   <React.StrictMode>
--- client/src/pages/StudentDashboard/StudentDashboard.js
+++ client/src/pages/StudentDashboard/StudentDashboard.js
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect, useState } from "react"
-import UserContext from "../contexts/UserContext";
+import UserContext from "../../contexts/UserContext";
 import axios from "axios";
-import { InputClear, FormBtn } from "../components/Form";
-import { Container, Col, Row } from "../components/Grid";
+import { InputClear, FormBtn } from "../../components/Form";
+import { Container, Col, Row } from "../../components/Grid";
 import { Link } from "react-router-dom";
-import ClassCard from "../components/ClassCard";
-import StudentGradeCard from "../components/StudentGradesCard";
-import coloredPencilsBottom from "../images/coloredPencilsBottom.jpg";
+import ClassCard from "../../components/ClassCard";
+import StudentGradeCard from "../../components/StudentGradesCard";
+// import coloredPencilsBottom from "../../public/images/coloredPencilsBottom.jpg";
+import "./style.css"
 
 function StudentDashboard() {
     const { id, prefix, firstName, lastName, userName, isTeacher } = useContext(UserContext)
@@ -27,12 +28,12 @@ function StudentDashboard() {
 
 
 return (
-    <div styles={{ backgroundImage: `url(${coloredPencilsBottom})` }}>
+    <div >
         <h2>Hello, {prefix} {firstName} {lastName}</h2>
         <h3>Username: {userName} </h3>
         <p>You {isTeacher ? "are" : "are not"} a teacher</p>
         <Row>
-            <Col size="md-6 sm-12">
+            <Col size="md-6 sm-12" id= "classCol">
                 <h2>My Classes</h2>
                 {classList.map((item) => (
                     <>
@@ -42,7 +43,7 @@ return (
                     </>
                 ))}
             </Col>
-            <Col size="md-6 sm-12">
+            <Col size="md-6 sm-12" id= "gradesCol">
                 <h2>My Grades</h2>
                 {classList.map((item) => (
                     <>
